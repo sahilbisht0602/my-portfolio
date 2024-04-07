3f91258c1ab8ba44b869ece2a4f0b9fbc4c0ca95
Sahil bisht <sahilbisht@Sahils-MacBook-Air.local>
2024-04-08 00:21:47 +0530

    title updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welcome to Sahil Portfolio",
+  title: "Welcome to Sahil's Portfolio",
   description:
     "Discover the craftsmanship of a frontend developer through a captivating portfolio showcasing innovative designs and seamless user experiences. Explore my projects and let's bring your ideas to life together.",
   icons: {
