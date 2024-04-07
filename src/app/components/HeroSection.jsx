ccedab4b1b9e66a15ad3ef3a688a12b87c243871
Sahil bisht <sahilbisht@Sahils-MacBook-Air.local>
2024-04-08 00:42:20 +0530

    build fix

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Let's Connect
+              Let&apos;s Connect
             </Link>
             <Link
               href="/SahilResume.pdf"
