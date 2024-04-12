88df169935cc221c63d71abadbc55cc27df27b80
Sahil bisht <sahilbisht@Sahils-MacBook-Air.local>
2024-04-12 12:17:51 +0530

    about me photo updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,9 +16,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:image" content="/images/ogimage.jpg" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
